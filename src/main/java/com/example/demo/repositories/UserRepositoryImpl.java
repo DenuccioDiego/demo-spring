81ff6a6fcb629b6f38405b4c29ba73311ba01efd
DenuccioDiego <diegodenuccio95@gmail.com>
2022-05-18 01:38:28 +0200

    iniezione del jdbc

--- src/main/java/com/example/demo/repositories/UserRepositoryImpl.java
+++ src/main/java/com/example/demo/repositories/UserRepositoryImpl.java
@@ -1,5 +1,29 @@
 package com.example.demo.repositories;
 
-public class UserRepositoryImpl {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.example.demo.database.JdbcConnectionFactory;
+import com.example.demo.entities.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    // Abilitare l'annotazione @Autowired per iniettare tramite l'annotazione
+    // (preferire Constructor injection)
+    // @Autowired
+    private final JdbcConnectionFactory jdbcConnectionFactory;
+
+    // Constructor Injection
+    public UserRepositoryImpl(JdbcConnectionFactory connectionFactory) {
+        this.jdbcConnectionFactory = connectionFactory;
+    }
+
+    @Override
+    public List<User> findUsers() throws Exception {
+        List<User> usersList = new ArrayList<User>();
+        // TODO Auto-generated method stub
+        return usersList;
+    }
 }

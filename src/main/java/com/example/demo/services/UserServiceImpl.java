d13653925cd73da90f8bbb984a1d7a106f188e53
DenuccioDiego <diegodenuccio95@gmail.com>
2022-05-18 02:18:02 +0200

    injection nell'implemetazione del service User

--- src/main/java/com/example/demo/services/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/UserServiceImpl.java
@@ -1,5 +1,27 @@
 package com.example.demo.services;
 
-public class UserServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> findUser() throws Exception {
+        List<User> userList = new ArrayList<User>();
+
+        // TODO implementare e richiamare metodo per trasformare il Vo in Dto
+
+        userRepository.findUsers();
+
+        return userList;
+    }
 }

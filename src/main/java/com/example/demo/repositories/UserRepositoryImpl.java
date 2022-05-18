0fe434f0f372a7eeb494d14abfbb4c6076e2e160
DenuccioDiego <diegodenuccio95@gmail.com>
2022-05-18 02:02:00 +0200

    query gestita nel repository

--- src/main/java/com/example/demo/repositories/UserRepositoryImpl.java
+++ src/main/java/com/example/demo/repositories/UserRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo.repositories;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +9,9 @@ import com.example.demo.database.JdbcConnectionFactory;
 import com.example.demo.entities.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserRepositoryImpl implements UserRepository {
 
     // Abilitare l'annotazione @Autowired per iniettare tramite l'annotazione
@@ -22,8 +26,16 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> findUsers() throws Exception {
-        List<User> usersList = new ArrayList<User>();
-        // TODO Auto-generated method stub
-        return usersList;
+        List<User> userList = new ArrayList<User>();
+
+        Statement stmt = jdbcConnectionFactory.openConnection().createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM public.'user' ORDER BY codicefiscale ASC");
+
+        while (rs.next()) {
+            System.out.println(rs.getString("nome"));
+            userList.add(new User());
+        }
+
+        return userList;
     }
 }
